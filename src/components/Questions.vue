fabb43406f846629eebc86e2272a2d9b194afe31
AM-dev <axelismv4630@gmail.com>
2022-05-09 10:01:00 +0200

    Class sur Questions

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -3,11 +3,11 @@
         <div class="accordion text-dark" id="accordionExample">
             <div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                     Pourquoi investir dans le crypto ?
                 </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, veritatis.
                     </div>
