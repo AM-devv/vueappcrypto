0ecc96c0c21b2a20bac821117aa2d918611101a4
AM-dev <axelismv4630@gmail.com>
2022-05-11 17:08:04 +0200

    Affichage favoris

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,21 @@
 <template>
-    <div class="vh-100">
+    <div class="container">
         <h2>Votre tableau de bord</h2>
-        <h3>cryptos favorites</h3>
-        <div>
-            <ul>
-                <li v-for="favorite in favorites" :key="favorite.id">
-                    {{ favorite.name }}
-                </li>
-            </ul>
+        <div class="row">
+            <h3>cryptos favorites</h3>
+            <h4 v-if="favorites == 0 || favorites == null">Aucun favoris</h4>
+            <div v-for="favorite in favorites" :key="favorite.id" class="card text-dark my-5 col-12 col-md-3">
+                    <h5 class="card-header">{{favorite.name}} <p @click="Removefav(favorite)" >🗑</p> </h5>
+                    <div class="card-body">
+                        <h5 class="card-title">{{favorite.current_price}}</h5>
+                        <p class="card-text">{{favorite.price_change_percentage_1h_in_currency}}</p>
+                        <router-link class="btn btn-primary btn-sm" :to="{ name: 'coininfo', params: {id: favorite.id} }">Plus d'info</router-link>
+                    </div>
+            </div>
         </div>
+            
+        
+        
     </div>
 </template>
 
@@ -22,6 +29,12 @@ export default {
     },
     created(){
         this.favorites = JSON.parse(localStorage.getItem('favorites'));
+    },
+    methods:{
+        Removefav(favorite){
+            this.favorites = this.favorites.filter(fav => fav.name !== favorite.name);
+            localStorage.setItem("favorites", JSON.stringify(this.favorites));
+        }
     }
 }
 </script>
