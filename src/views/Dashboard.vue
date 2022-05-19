63a003cc3111a5e85944227d8ea58c894c43cf43
AM-dev <axelismv4630@gmail.com>
2022-05-19 14:45:22 +0200

    Dashboard favoris card

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -17,19 +17,21 @@
                 </div>
                 <h4 v-if="messages == null">Aucun message</h4>
             </section>
-            <section class="row col-lg-6 col-12">
+            <section class="col-lg-6 col-12">
             <h3 v-if="favorites != null">Cryptos favorites {{ favorites.length }}</h3>
             <h4 v-if="favorites == 0 || favorites == null">Aucun favoris</h4>
-            <div v-for="favorite in favorites" :key="favorite.id" class="card shadow text-dark my-5 mx-1 col-12 col-lg-3">
+            <div class="bg-light rounded row justify-content-center">
+                <div v-for="favorite in favorites" :key="favorite.id" class="card shadow bg-dark text-light my-3 mx-1 col-10 col-md-4 col-lg-3">
                     <h5 class="card-header">{{favorite.name}}</h5>
                     <div class="card-body">
                         <router-link class="btn btn-primary btn-sm" :to="{ name: 'coininfo', params: {id: favorite.id} }">Plus d'infos</router-link>
                         <hr>
                         <button class="btn btn-danger btn-sm" @click="Removefav(favorite)">Supprimer</button>
                     </div>
+                </div>
             </div>
         </section>
-        <section class="row  col-lg-6 col-12">
+        <section class="col-lg-6 col-12">
             <h3>Top 7 tendances</h3>
             <trending></trending>
             
