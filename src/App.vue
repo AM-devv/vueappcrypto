9e1b935c12e51e73713ca1852c457a82dea24a2a
AM-dev <axelismv4630@gmail.com>
2022-05-23 14:40:31 +0200

    Background

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class=" space text-light h-100">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <img src="./assets/logovuecrp.png" width="250" alt="logo">
@@ -50,7 +50,12 @@ export default {
 
 <style scoped>
 .space{
-background-color: #0E6DF5;
-background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='2000' height='2000' viewBox='0 0 20 20'%3E%3Cg %3E%3Cpolygon fill='%231D1D1D' points='20 10 10 0 0 0 20 20'/%3E%3Cpolygon fill='%231D1D1D' points='0 10 0 20 10 20'/%3E%3C/g%3E%3C/svg%3E");
+/* background-color: #0E6DF5;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='2000' height='2000' viewBox='0 0 20 20'%3E%3Cg %3E%3Cpolygon fill='%231D1D1D' points='20 10 10 0 0 0 20 20'/%3E%3Cpolygon fill='%231D1D1D' points='0 10 0 20 10 20'/%3E%3C/g%3E%3C/svg%3E"); */
+
+background-color: #000000;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23140227'/%3E%3Cstop offset='1' stop-color='%23140227' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23004586'/%3E%3Cstop offset='1' stop-color='%23004586' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%230306aa'/%3E%3Cstop offset='1' stop-color='%230306aa' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23000000'/%3E%3Cstop offset='1' stop-color='%23000000' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%2327034D'/%3E%3Cstop offset='1' stop-color='%2327034D' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%230C8AFF'/%3E%3Cstop offset='1' stop-color='%230C8AFF' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E");
+background-attachment: fixed;
+background-size: cover;
 }
 </style>
