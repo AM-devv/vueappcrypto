d483fcad1e71f7632ddb060cdbb63d9e8fb74e0a
AM-dev <axelismv4630@gmail.com>
2022-05-30 13:26:38 +0200

    More posts

--- src/components/Lastposts.vue
+++ src/components/Lastposts.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="bg-light shadow rounded text-dark p-3">
             <h3>Tous les posts likés</h3>
-            <div v-for="post in postsliked" :key="post.id" class="card my-3 shadow text-dark">
+            <div v-for="post in postsliked.slice(0,limit)" :key="post.id" class="card my-3 shadow text-dark">
                 <div class="card-header">
                     {{post.name}}
                 </div>
@@ -12,6 +12,12 @@
                 </div>
             </div>
             <p v-if="postsliked.length == 0">Aucuns messages likés</p>
+            <div class="text-center" v-else>
+                <button class="btn btn-transparent rounded-pill" @click="limit = limit + 3" >
+                    <strong>...</strong>
+                </button>
+            </div>
+            
         </div>
     </div>
 </template>
@@ -22,7 +28,9 @@ export default {
     name: "Lastposts",
     data(){
         return{
-            postsliked : JSON.parse(localStorage.getItem('postsliked')) || []
+            postsliked : JSON.parse(localStorage.getItem('postsliked')) || [],
+
+            limit : 3
         }
     }
 }
