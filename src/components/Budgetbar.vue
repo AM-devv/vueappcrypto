2beb2ab95322a9adab3a4878e22c318dac344257
AM-dev <axelismv4630@gmail.com>
2022-11-02 00:06:57 +0100

    Budgetbar tooltip

--- src/components/Budgetbar.vue
+++ src/components/Budgetbar.vue
@@ -2,10 +2,11 @@
     <div>
         <div class="bg-light shadow rounded text-dark p-3">
             <h3>Représentation investissements</h3>
-            <h5>Sur {{ sum }} voici vos investissements</h5>
+            <h5>Sur {{ sum }} ☾ voici vos investissements</h5>
             <div class="w-100 d-flex">
                 <div class="text-center p-1 investcolor" :style="{backgroundColor: '#'+(Math.random()*0xFFFFFF<<0).toString(16), width:((item.invest/sum)*100)+ '%'}" v-for="item in boiteinvest" :key="item.id">
                    <img :src="item.image" alt="cryptologo">
+                   <span class="tooltiptext text-light rounded p-1">{{ item.invest }} ☾</span>
                 </div>
             </div>
         </div>
@@ -39,8 +40,26 @@ export default {
     transition: all .2s ease-in-out;
 }
 
-.investcolor:hover{
-    scale: 1.5;
+.tooltiptext{
+    display: none;
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.5);
+    margin-top: 0.5rem;
+}
+
+.investcolor:hover .tooltiptext{
+    display: block;
+}
+
+.tooltiptext::after {
+  content: " ";
+  position: absolute;
+  bottom: 100%;  
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent rgba(0, 0, 0, 0.5) transparent;
 }
 
 .investcolor:first-child{
